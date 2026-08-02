6c2402a45ae5906da33923339d881dbc0e66034f
fedesan2002 <fedesan2002>
2026-08-02 19:21:47 -0300

    Ajustar declaraciones const y let segun rubrica de evaluacion

--- js/main.js
+++ js/main.js
@@ -5,17 +5,17 @@ alert("¡Bienvenido a la Comiquería Central!");
 let seguirComprando = true;
 
 while (seguirComprando) {
-    let nombreCliente = prompt("Ingrese su nombre:");
-    let nombreComic = prompt("¿Qué cómic querés comprar?");
-    let cantidad = parseInt(prompt("¿Cuántas unidades querés?"));
-    let precioUnitario = parseFloat(prompt("¿Cuál es el precio unitario?"));
+    const nombreCliente = prompt("Ingrese su nombre:");
+    const nombreComic = prompt("¿Qué cómic querés comprar?");
+    const cantidad = parseInt(prompt("¿Cuántas unidades querés?"));
+    const precioUnitario = parseFloat(prompt("¿Cuál es el precio unitario?"));
 
     // Validacion para que no ingrese datos vacios o incorrectos
     if (isNaN(cantidad) || isNaN(precioUnitario) || cantidad <= 0 || precioUnitario <= 0) {
         alert("Por favor ingresá números válidos para la cantidad y el precio.");
     } else {
-        let totalCompra = cantidad * precioUnitario;
-        let mensaje = "Hola " + nombreCliente + "! El total por " + cantidad + " unidad/es de '" + nombreComic + "' es $" + totalCompra;
+        const totalCompra = cantidad * precioUnitario;
+        const mensaje = "Hola " + nombreCliente + "! El total por " + cantidad + " unidad/es de '" + nombreComic + "' es $" + totalCompra;
         
         alert(mensaje);
         console.log(mensaje);
